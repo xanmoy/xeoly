d0e3e09313aaea3536bbde6b6071529ef5add4e8
Tanmoy Ganguly <contact@xanmoy.me>
2024-05-15 03:47:42 +0530

    updated

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,21 +1,41 @@
-export const dynamic = "force-dynamic";
-
 import WebSearchResults from "@/components/WebSearchResults";
 import Link from "next/link";
 
+export const dynamic = "force-dynamic";
+
 export default async function WebSearchPage({ searchParams }) {
   const startIndex = searchParams.start || "1";
+
+  // Simulate a delay
   await new Promise((resolve) => setTimeout(resolve, 2000));
-  const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}}&start=${startIndex}`
-  );
 
-  if (!response.ok) {
-    console.log(response);
-    throw new Error("Something went wrong");
+  let data;
+  try {
+    const response = await fetch(
+      `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${startIndex}`
+    );
+
+    if (!response.ok) {
+      console.error('Fetch error:', response.status, response.statusText);
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    data = await response.json();
+
+  } catch (error) {
+    console.error('Something went wrong:', error);
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">Something went wrong</h1>
+        <p className="text-lg">
+          Please try again later or go back to the homepage{" "}
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
   }
-
-  const data = await response.json();
 
   const results = data.items;
 
@@ -32,5 +52,6 @@ export default async function WebSearchPage({ searchParams }) {
       </div>
     );
   }
+
   return <>{results && <WebSearchResults results={data} />}</>;
 }
