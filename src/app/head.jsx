1c6c97964fc9bbb37f2a7f0dd11b6f405cc6f848
xanmoy <xanmoy.tsx@gmail.com>
2025-01-08 01:31:06 +0530

    updated

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -23,8 +23,26 @@ export default function Head() {
         href="/favicon-16x16.png"
       />
       <link rel="manifest" href="/site.webmanifest" />
+      <meta name="keywords" content="Xeoly, xanmoy, Tanmoy Ganguly, Software Engineer, search engine"/>
+      <meta name="author" content="Tanmoy Ganguly"/>
+      <meta name="msapplication-TileColor" content="#4f46e5" />
       <meta name="theme-color" content="#4f46e5" />
       <meta name="robots" content="index,follow,archive" />
+      <meta name="googlebot" content="index,follow,archive" />
+      <meta name="bingbot" content="index,follow,archive" />
+      <meta name="yandex" content="index,follow,archive" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@xanmoy" />
+      <meta name="twitter:creator" content="@xanmoy" />
+      <meta property="og:site_name" content="Xeoly" />
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:url" content="https://xeoly,xanmoy.in" />
+      <meta property="og:image" content="/xeoly-social.png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:image:alt" content="Xeoly Secure Search Engine" />
+      
     </>
   );
 }
